98ae69ef4bc2c61725a84d222278c32200387341
fthiounn <fthiounn@gmail.com>
2019-11-15 16:54:29 +0100

    update

--- lib/mkdirails.rb
+++ lib/mkdirails.rb
@@ -5,9 +5,9 @@ def perform
 	#setting the content of the files we will create
 	check_if_user_gave_input
 	repo_name = get_user_input
-  gitignore = ".env\n.DS_Store"
+  gitignore = ".env\n.DS_Store\npublic/assets"
   gems = "#gem for this project\ngem 'font-awesome-sass', '~> 5.11.2'\ngem 'bcrypt'\ngem 'table_print'\ngem 'faker'\ngem 'dotenv-rails'\ngem 'devise'"
-
+  assets = "Rails.application.config.assets.paths << Rails.root.join('lib', 'assets', 'stylesheets')\nRails.application.config.assets.paths << Rails.root.join('lib', 'assets', 'javascripts')\nRails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets')\nRails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')"
 	#Creating the git repo and initialization of gemfile and rspec
 	system " rails _5.2.3_ new -d postgresql #{repo_name} "
 	Dir.chdir("#{repo_name}")
@@ -20,6 +20,12 @@ def perform
   open('Gemfile', 'a') { |f|
   f.puts gems
   }
+  #adding assets setup
+  open('config/initializers/assets.rb', 'a') { |f|
+  f.puts assets
+  }
+  system 'mkdir lib/assets/ vendor/assets/ '
+  system 'mkdir lib/assets/stylesheets/ lib/assets/javascripts/ vendor/assets/stylesheets/ vendor/assets/javascripts/'
 	system 'bundle install'
 	system 'rspec --init'
 	system 'git init'
